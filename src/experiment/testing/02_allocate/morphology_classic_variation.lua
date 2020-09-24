31c9aabab51eb6a3c973889beae573016347f3e6
Weixu Zhu <freedomcondor@126.com>
2020-09-24 22:12:23 +0200

    merge new_allocator with stable new allocator structure

--- src/experiment/testing/02_allocate/morphology_classic_variation.lua
+++ src/experiment/testing/02_allocate/morphology_classic_variation.lua
@@ -38,7 +38,7 @@ return
 			positionV3 = vector3(0, -dis*2, 0),
 			orientationQ = quaternion(math.pi/2, vector3(0,0,1)),
 			children = {
-			{	robotTypeS = "drone",
+			{	robotTypeS = "pipuck",
 				positionV3 = vector3(-dis, -dis, 0),
 				orientationQ = quaternion(0, vector3(0,0,1)),
 			},
@@ -60,7 +60,7 @@ return
 			positionV3 = vector3(0, dis*2, 0),
 			orientationQ = quaternion(-math.pi/2, vector3(0,0,1)),
 			children = {
-			{	robotTypeS = "drone",
+			{	robotTypeS = "pipuck",
 				positionV3 = vector3(-dis, -dis, 0),
 				orientationQ = quaternion(0, vector3(0,0,1)),
 			},
