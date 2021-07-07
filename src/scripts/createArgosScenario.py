67ace7420ba7e6e39b2cde9f1d1ea4c2c0af3e32
Weixu Zhu <zhuweixu_harry@126.com>
2021-07-07 18:27:16 +0200

    add generate random and robot xml scripts

--- src/scripts/createArgosScenario.py
+++ src/scripts/createArgosScenario.py
@@ -1,4 +1,16 @@
-def create_real_scenario_object() :
+import random
+import sys
+
+#######################################################################
+# random seed
+Inputseed = 1
+if len(sys.argv) >= 2 :
+	Inputseed = sys.argv[1]
+random.seed(Inputseed)
+
+#######################################################################
+# real lab scenario
+def generate_real_scenario_object() :
 	text = '''
 	<!-- room -->
 	<!--
@@ -80,8 +92,263 @@ def create_real_scenario_object() :
 	'''
 	return text
 
+#######################################################################
+# add xml of drone, pipuck, obstacle, targets
+def generate_obstacle_xml(i, x, y, type):
+	tag = '''
+	<block id="obstacle{}" init_led_color="{}">
+		<body position="{},{},0" orientation="0,0,0" />
+		<controller config="block"/>
+	</block>
+	'''.format(i, type, x, y)
+	return tag
 
-def create_argos_file(template_name, argos_name, replacements) :
+def generate_drone_xml(i, x, y) :
+	tag = '''
+	<drone id="drone{}">
+		<body position="{},{},0" orientation="0,0,0"/>
+		<controller config="drone"/>
+	</drone>
+	'''.format(i, x, y)
+	return tag
+
+def generate_pipuck_xml(i, x, y) :
+	tag = '''
+	<pipuck id="pipuck{}" wifi_medium="wifi" tag_medium="tags">
+		<body position="{},{},0" orientation="0,0,0"/>
+		<controller config="pipuck"/>
+	</pipuck>
+	'''.format(i, x, y)
+	return tag
+
+def generate_target_xml(x, y, radius, tag_edge_distance):
+	tag = '''
+	<prototype id="target" movable="true" friction="2">
+		<body position="{},{},0" orientation="0,0,0" />
+		<links ref="base">
+			<link id="base" geometry="cylinder" radius="{}" height="0.1" mass="0.01"
+			      position="0,0,0" orientation="0,0,0" />
+			</links>
+		<devices>
+			<tags medium="tags">
+				<tag anchor="base" observable_angle="75" side_length="0.02"
+				     position="{},0.000,0.11" orientation="0,0,0" />
+			</tags>
+		</devices>
+    </prototype>
+	'''.format(x, y, radius, tag_edge_distance-radius)
+	return tag
+
+#######################################################################
+# generate random locations
+
+# from <left_end> to <right_end>, generate <gate_number> gates of different sizes ranging from <small_limit> and <large_limit>
+# return an array of gates in order
+#	[
+#		[left1, right1],
+#		[left2, right2],
+#	]
+#	and the middle of the largest gate
+def generate_gate_locations(gate_number, left_end, right_end, small_limit, large_limit) :
+	a = []
+	largest_length = 0
+	largest_loc = 0
+	for i in range(1, gate_number+1) :
+		valid_position = False
+		while valid_position == False:
+			loc = left_end + random.random() * (right_end - left_end)
+			size = small_limit + random.random() * (large_limit - small_limit)
+			if i == 1 :
+				size = large_limit
+			left = loc - size/2 
+			right = loc + size/2 
+
+			if left < left_end or right > right_end :
+				valid_position = False
+				continue
+
+			valid_position = True
+			for item in a:
+				if (item[0] < left and right < item[1] or
+				    left < item[0] and item[0] < right or
+				    left < item[1] and item[1] < right
+				   ) :
+					valid_position = False
+					break
+
+			if valid_position == True:
+				# push into list
+				a.append([left,right])
+				# check largest
+				if right - left > largest_length :
+					largest_length = right - left
+					largest_loc = (left + right) / 2
+
+	#sort
+	for i in range(0, gate_number-1) :
+		for j in range(i+1, gate_number) :
+			if a[i][0] > a[j][0] :
+				temp = a[i]
+				a[i] = a[j]
+				a[j] = temp
+	return a, largest_loc
+
+# from <left_end> to <right_end>, generate <gate_number> gates of different sizes ranging from <small_limit> and <large_limit>
+# fill the wall with <step>
+# return an array of gates in order
+#	[
+#		[1D-location, type],
+#	]
+#	and the middle of the largest gate
+def generate_block_locations(gate_number, left_end, right_end, small_limit, large_limit, step) :
+	block_locations = []
+	margin = 0.10
+	gate_locations, largest_loc = generate_gate_locations(gate_number, left_end + margin, right_end - margin, small_limit, large_limit)
+	for i in range(0, gate_number + 1) :
+		# get interval of gates
+		left = left_end
+		right = right_end
+		if i != 0 :
+			left = gate_locations[i-1][1]
+		if i != gate_number :
+			right = gate_locations[i][0]
+
+		#set blocks from left to right
+		j = left
+		while j < right - 0.055 :
+			block_locations.append([j, "blue"])
+			j = j + step
+		block_locations.append([right, "blue"])
+		#set mark blocks
+		if i != 0 :
+			block_locations.append([left - 0.06, "orange"])
+		if i != gate_number :
+			block_locations.append([right + 0.06, "orange"])
+		
+	return block_locations, largest_loc
+
+# from <left_end> to <right_end>, generate <gate_number> gates of different sizes ranging from <small_limit> and <large_limit>
+# fill the wall with <step>
+# return an array of gates in order
+#	[
+#		[1D-location, type],
+#	]
+#	and the middle of the largest gate
+def generate_wall(gate_number, wall_x, left_end, right_end, small_limit, large_limit, step) :
+	tagstr = ""
+	block_locations, largest_loc = generate_block_locations(gate_number, left_end, right_end, small_limit, large_limit, step) 
+	i = 0
+	for loc in block_locations :
+		i = i + 1
+		tagstr = tagstr + generate_obstacle_xml(i, wall_x, loc[0], loc[1])
+
+	return tagstr, largest_loc
+
+#- random locations ------------------------------------------------------------------------
+def generate_random_locations(n, origin_x,    origin_y, 
+                                 x_min_limit, x_max_limit,
+                                 y_min_limit, y_max_limit, 
+                                 near_limit,  far_limit) :
+	a = []
+
+	# if origin is not None then add origin as the first
+	start = 0
+	if origin_x != None and origin_y != None :
+		a.append([origin_x, origin_y])
+		start = 1
+
+	# start generating
+	for i in range(start, n) : # 0/1 to n - 1
+		valid = False
+		while valid == False :
+			loc_x = x_min_limit + random.random() * (x_max_limit - x_min_limit)
+			loc_y = y_min_limit + random.random() * (y_max_limit - y_min_limit)
+
+			#check near
+			valid = True
+			for j in range(0, i) :
+				if (loc_x - a[j][0]) ** 2 + (loc_y - a[j][1]) ** 2 < near_limit ** 2 :
+					valid = False
+					break
+			if valid == False :
+				continue
+
+			#check faraway
+			valid = False
+			if i == 0 :
+				valid = True
+			for j in range(0, i) :
+				if (loc_x - a[j][0]) ** 2 + (loc_y - a[j][1]) ** 2 < far_limit ** 2 :
+					valid = True
+					break
+			if valid == True :
+				a.append([loc_x, loc_y])
+	return a
+
+def generate_slave_locations(n, master_locations,
+                                x_min_limit, x_max_limit,
+                                y_min_limit, y_max_limit, 
+                                near_limit, far_limit) :
+	a = []
+	for i in range(0, n) :
+		valid = False
+		while valid == False :
+			loc_x = x_min_limit + random.random() * (x_max_limit - x_min_limit)
+			loc_y = y_min_limit + random.random() * (y_max_limit - y_min_limit)
+
+			# check near
+			valid = True
+			for j in range(0, i) :
+				if (loc_x - a[j][0]) ** 2 + (loc_y - a[j][1]) ** 2 < near_limit ** 2 :
+					valid = False
+					break
+			if valid == False :
+				continue
+
+			#check faraway
+			valid = False
+			for drone_loc in drone_locations :
+				if (loc_x - drone_loc[0]) ** 2 + (loc_y - drone_loc[1]) ** 2 < far_limit ** 2 :
+					valid = True
+					break
+			if valid == True :
+				a.append([loc_x, loc_y])
+	return a
+
+def generate_drones(locations, start_id) :
+	tagstr = ""
+	i = start_id
+	for loc in locations :
+		tagstr = tagstr + generate_drone_xml(i, loc[0], loc[1])
+		i = i + 1
+	return tagstr
+
+def generate_pipucks(locations, start_id) :
+	tagstr = ""
+	i = start_id
+	for loc in locations :
+		tagstr = tagstr + generate_pipuck_xml(i, loc[0], loc[1])
+		i = i + 1
+	return tagstr
+
+def generate_obstacles(locations, start_id, type) :
+	tagstr = ""
+	i = start_id
+	for loc in locations :
+		tagstr = tagstr + generate_obstacle_xml(i, loc[0], loc[1], type)
+		i = i + 1
+	return tagstr
+#######################################################################
+# create argos file
+def generate_argos_file(template_name, argos_name, replacements) :
+	'''
+	replacements = 
+	[
+		[RANDOMSEED, str(500)],
+		[xxx, yyy],
+		[xxx, yyy],
+	]
+	'''
 	with open(template_name, 'r') as file :
 		filedata = file.read()
 
--- src/testing/exp_01_real_scenario/run.in.py
+++ src/testing/exp_01_real_scenario/run.in.py
@@ -4,12 +4,51 @@ exec(compile(open(createArgosFileName, "rb").read(), createArgosFileName, 'exec'
 
 import os
 
-create_argos_file("@CMAKE_CURRENT_BINARY_DIR@/vns_template.argos", 
-                  "@CMAKE_CURRENT_BINARY_DIR@/vns.argos",
+# drone and pipuck
+drone_locations = generate_random_locations(3,                  # total number
+                                            -3, 0,              # origin location
+                                            -5, -3,             # random x range
+                                            -2, 2,              # random y range
+											0.5, 1.0)           # near limit and far limit
+pipuck_locations = generate_slave_locations(8,
+                                            drone_locations,
+                                            -5, -3,             # random x range
+                                            -2, 2,              # random y range
+											0.5, 1.0)           # near limit and far limit
+drone_xml = generate_drones(drone_locations, 1)                 # from lable 1 generate drone xml tags
+pipuck_xml = generate_pipucks(pipuck_locations, 1)              # from lable 1 generate pipuck xml tags
+
+# wall
+wall_xml, largest_loc = generate_wall(1,                        # number of gates
+                                      0,                        # x location of the wall
+                                      -2.9, 2.9,                # y range of the wall
+                                      0.5, 1.5,                 # size range of the gate
+                                      0.15)                     # block distance to fill the wall
+
+# obstacles
+obstacle_locations = generate_random_locations(5,               # total number
+                                               None, None,      # origin location
+                                               -2.5, -0.5,      # x range
+                                               -1.0, 1.0,       # y range
+                                               0.0, 3.0)          # near and far limit
+
+obstacle_xml = generate_obstacles(obstacle_locations, 100, "black")
+
+# target
+target_xml = generate_target_xml(3.5, largest_loc, 0.3, 0.3)
+
+# generate argos file
+generate_argos_file("@CMAKE_CURRENT_BINARY_DIR@/vns_template.argos", 
+                    "@CMAKE_CURRENT_BINARY_DIR@/vns.argos",
 	[
-		["RANDOMSEED", str(1)],
-		["TOTALLENGTH", str(1000)],
-		["REAL_SCENARIO", create_real_scenario_object()],
+		["RANDOMSEED",    str(1)],
+		["TOTALLENGTH",   str(1000)],
+		["REAL_SCENARIO", generate_real_scenario_object()],
+		["DRONES",        drone_xml], 
+		["PIPUCKS",       pipuck_xml], 
+		["WALL",          wall_xml], 
+		["OBSTACLES",     obstacle_xml], 
+		["TARGET",        target_xml], 
 	]
 )
 
