216afb601332a17de79afe543e1190da8ad9c7c8
Weixu Zhu <freedomcondor@126.com>
2021-08-23 00:01:41 +0200

    Remove pipuck based stablizer

--- src/core/vns/Stabilizer.lua
+++ src/core/vns/Stabilizer.lua
@@ -38,6 +38,17 @@ function Stabilizer.preStep(vns)
 end
 
 function Stabilizer.postStep(vns)
+	--[[
+	if vns.robotTypeS == "pipuck" and vns.parentR ~= nil then
+		for _, msgM in pairs(vns.Msg.getAM(vns.parentR.idS, "you_are_a_reference")) do
+			vns.Msg.send(vns.parentR.idS, "reference_estimated_location_report", 
+			{
+				positionV3 = vns.api.virtualFrame.V3_VtoR(vns.api.estimateLocation.positionV3),
+				orientationQ = vns.api.virtualFrame.Q_VtoR(vns.api.estimateLocation.orientationQ),
+			})
+		end
+	end
+	--]]
 end
 
 function Stabilizer.step(vns)
@@ -52,6 +63,15 @@ function Stabilizer.step(vns)
 		-- in A's eye, I'm at
 		--   position:     (-A.positionV3):rotate(A.orientationQ:inverse())
 		--   orientation:  A.orientationQ:inverse()
+
+		--[[
+			A.positionV3, A.orientationQ
+			myVec in A's eye
+				(myVec-A.positionV3):rotate(A.orientationQ:inverse())
+			myQua in A's eye
+				A.orientationQ:inverse() * myQua
+		--]]
+	
 
 		-- find a reference
 		local current_reference = Stabilizer.getNearestReference(vns)
@@ -68,10 +88,49 @@ function Stabilizer.step(vns)
 		vns.stabilizer.reference = current_reference
 
 		-- if it is not stable
-		--      ask and receive speed from it
+		--      ask and receive estimated location from it
 		--      add it to reference_offset
+		--[[
 		if vns.stabilizer.reference.robotTypeS == "pipuck" then
+			vns.Msg.send(vns.stabilizer.reference.idS, "you_are_a_reference")
+			for _, msgM in pairs(vns.Msg.getAM(vns.stabilizer.reference.idS, "reference_estimated_location_report")) do
+				--reference_offset in estimated location's eye
+				vns.stabilizer.reference_offset.positionV3 = 
+					vector3(vns.stabilizer.reference_offset.positionV3 - msgM.dataT.positionV3):rotate(
+						msgM.dataT.orientationQ:inverse()
+					)
+				vns.stabilizer.reference_offset.orientationQ = 
+					msgM.dataT.orientationQ:inverse() *
+					vns.stabilizer.reference_offset.orientationQ
+			end
 		end
+		--]]
+
+		--draw reference
+		vns.api.debug.drawArrow("red", 
+		    vns.api.virtualFrame.V3_VtoR(vns.stabilizer.reference.positionV3), 
+		    vns.api.virtualFrame.V3_VtoR(
+				vns.stabilizer.reference.positionV3 + vector3(vns.stabilizer.reference_offset.positionV3):rotate(
+					vns.stabilizer.reference.orientationQ
+				)
+			)
+		)
+
+		vns.api.debug.drawArrow("red", 
+		    vns.api.virtualFrame.V3_VtoR(
+				vns.stabilizer.reference.positionV3 + vector3(vns.stabilizer.reference_offset.positionV3):rotate(
+					vns.stabilizer.reference.orientationQ
+				)
+			),
+
+		    vns.api.virtualFrame.V3_VtoR(
+				vns.stabilizer.reference.positionV3 + vector3(vns.stabilizer.reference_offset.positionV3):rotate(
+					vns.stabilizer.reference.orientationQ
+				) 
+				+ 
+				vector3(0.2, 0, 0):rotate(vns.stabilizer.reference.orientationQ * vns.stabilizer.reference_offset.orientationQ)
+			)
+		)
 
 		-- estimate location of the new step 
 		-- based on spreader.spreading_speed
@@ -84,20 +143,15 @@ function Stabilizer.step(vns)
 
 		-- accumulate step_offset to reference_offset
 		-- offset in reference obstacle's eye
-		local step_offset_in_ref = {
-			positionV3 = vector3(-vns.stabilizer.step_offset.positionV3):rotate(vns.stabilizer.step_offset.orientationQ:inverse()),
-			orientationQ = vns.stabilizer.step_offset.orientationQ:inverse()
-		}
 		vns.stabilizer.reference_offset.positionV3 = 
-			vns.stabilizer.reference_offset.positionV3 + vector3(step_offset_in_ref.positionV3):rotate(
-				vns.stabilizer.reference_offset.orientationQ
-			)
+			vns.stabilizer.reference_offset.positionV3 + 
+			vector3(vns.stabilizer.step_offset.positionV3):rotate(vns.stabilizer.reference_offset.orientationQ)
 		vns.stabilizer.reference_offset.orientationQ = vns.stabilizer.reference_offset.orientationQ * 
-		                                               step_offset_in_ref.orientationQ
+		                                               vns.stabilizer.step_offset.orientationQ
 
 		-- accumulate reference_offset to goal
 		vns.goal.positionV3 = vns.stabilizer.reference.positionV3 + 
-		                      vector3(vns.stabilizer.reference_offset.positionV3):rotate(vns.stabilizer.reference_offset.orientationQ)
+		                      vector3(vns.stabilizer.reference_offset.positionV3):rotate(vns.stabilizer.reference.orientationQ)
 		vns.goal.orientationQ = vns.stabilizer.reference.orientationQ * vns.stabilizer.reference_offset.orientationQ
 		-- tell allocator to use vns.goal for the brain
 		vns.stabilizer.allocator_signal = true
@@ -115,15 +169,18 @@ function Stabilizer.getNearestReference(vns)
 			nearest = obstacle
 		end
 	end
+	--[[
 	if nearest == nil then
 		local distance = math.huge
 		for idS, robotR in pairs(vns.connector.seenRobots) do
-			if robotR.positionV3:length() < distance then
+			if robotR.robotTypeS == "pipuck" and
+			   robotR.positionV3:length() < distance then
 				distance = robotR.positionV3:length()
 				nearest = robotR 
 			end
 		end
 	end
+	--]]
 	return nearest
 end
 
