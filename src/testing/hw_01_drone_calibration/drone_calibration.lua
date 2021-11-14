fd004e4ed49c3f6438dc1433e1f64824236c8c1f
freedomcondor <zhuweixu_harry@126.com>
2021-11-14 17:27:35 +0100

    get position and orientations for camera calibration

--- src/testing/hw_01_drone_calibration/drone_calibration.lua
+++ src/testing/hw_01_drone_calibration/drone_calibration.lua
@@ -1,3 +1,30 @@
+function ShowTable(table, number, skipindex)
+	-- number means how many indents when printing
+	if number == nil then number = 0 end
+	if type(table) ~= "table" then return nil end
+ 
+	for i, v in pairs(table) do
+	   local str = "DebugMSG:\t\t"
+	   for j = 1, number do
+		  str = str .. "\t"
+	   end
+ 
+	   str = str .. tostring(i) .. "\t"
+ 
+	   if i == skipindex then
+		  print(str .. "SKIPPED")
+	   else
+		  if type(v) == "table" then
+			 print(str)
+			 ShowTable(v, number + 1, skipindex)
+		  else
+			 str = str .. tostring(v)
+			 print(str)
+		  end
+	   end
+	end
+end
+
 function init()
 	for id, camera in pairs(robot.cameras_system) do    
 		camera.enable()
@@ -13,12 +40,41 @@ local reference_tags = {
 	{id = 4, position = vector3( dis,-dis,-height), orientation = quaternion()},
 }
 
+-- reverse calculation
+-- A.positionV3, A.orientationQ
+-- in A's eye, I'm at
+--   position:     (-A.positionV3):rotate(A.orientationQ:inverse())
+--   orientation:  A.orientationQ:inverse()
+
 function step()
 	-- for each camera
 	for id, camera in pairs(robot.cameras_system) do 
+		local calibrated = false
 		-- for each tag
-		for 
+		for _, tag in ipairs(camera.tags) do
+			-- for each reference_tag
+			for _, reference_tag in ipairs(reference_tags) do
+				if tag.id == reference_tag.id then
+					local camera_to_tag = {
+						position = (-tag.position):rotate(tag.orientation:inverse()),
+						orientation = tag.orientation:inverse(),
+					}
+					camera.camera_to_robot = {
+						position = reference_tag.position + camera_to_tag.position:rotate(reference_tag.orientation),
+						orientation = reference_tag.orientation * camera_to_tag.orientation,
+					}
+					calibrated = true
+					break
+				end
+			end
+			if calibrated == true then
+				break
+			end
+		end
 	end
+
+	ShowTable("cameras_system")
+	ShowTable(robot.cameras_system)
 end
 
  
