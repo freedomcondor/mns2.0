d2a53342a1687bbfd90973344b786d35a3ed6f46
Weixu Zhu <zhuweixu_harry@126.com>
2022-02-02 19:50:27 +0100

    Tweak parameters based on reality

--- src/core/vns/Parameters.lua
+++ src/core/vns/Parameters.lua
@@ -3,12 +3,12 @@ return {
 	-- recruit happens only if robots in the safezone
 	-- robots wait if neighbours outside the safezone
 	safezone_default = tonumber(robot.params.safezone_default or 1.00), -- for unknown robot type
-	safezone_drone_drone = tonumber(robot.params.safezone_drone_drone or 1.50),
+	safezone_drone_drone = tonumber(robot.params.safezone_drone_drone or 1.70),
 	safezone_drone_pipuck = tonumber(robot.params.safezone_drone_pipuck or 1.00),
 	safezone_pipuck_pipuck = tonumber(robot.params.safezone_pipuck_pipuck or 1.00),
 
 	-- robots avoid each other within dangerzone
-	dangerzone_drone = tonumber(robot.params.dangerzone_drone or 1.00),
+	dangerzone_drone = tonumber(robot.params.dangerzone_drone or 1.50),
 	dangerzone_pipuck = tonumber(robot.params.dangerzone_pipuck or 0.40),
 	dangerzone_block = tonumber(robot.params.dangerzone_block or 0.40),
 	dangerzone_predator = tonumber(robot.params.dangerzone_predator or 0.50),
@@ -45,15 +45,16 @@ return {
 	driver_default_rotate_scalar = tonumber(robot.params.driver_default_rotate_scalar or 0.3),
 
 	-- time out
-	connector_waiting_count = tonumber(robot.params.connector_waiting_count or 3),
+	connector_waiting_count = tonumber(robot.params.connector_waiting_count or 7),
 		-- robots wait this waiting_count steps for ack after recruiting a robot
-	connector_waiting_parent_count = tonumber(robot.params.connector_waiting_parent_count or 5),
+	connector_waiting_parent_count = tonumber(robot.params.connector_waiting_parent_count or 10),
 		-- robots wait this waiting_parent_count steps for a parent recuit again
-	connector_unseen_count = tonumber(robot.params.connector_unseen_count or 3),
+	connector_unseen_count = tonumber(robot.params.connector_unseen_count or 7),
 		-- robots disconnect after this steps after losing visual of a robot
 	connector_heartbeat_count = tonumber(robot.params.connector_heartbeat_count or 3),
 		-- robots disconnect after this steps after stop receiving hearbeat from a neighbour (parent + children)
 
 	brainkeeper_time = tonumber(robot.params.brainkeeper_time or 100), 
 	stabilizer_preference_robot = robot.params.stabilizer_preference_robot or "pipuck1",
+	stabilizer_pipuck_reference_distance = tonumber(robot.params.stabilizer_pipuck_reference_distance or 0.25)
 }
