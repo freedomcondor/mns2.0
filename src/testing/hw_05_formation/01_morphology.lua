01ee14ff8f0d2280d50ef89667309ba6521a3557
Weixu Zhu <freedomcondor@126.com>
2022-03-02 11:10:01 +0100

    Adjust formation hw 05 test

--- src/testing/hw_05_formation/01_morphology.lua
+++ src/testing/hw_05_formation/01_morphology.lua
@@ -1,4 +1,5 @@
 local dis = 0.5
+local height = 1.8
 return 
 {	robotTypeS = "drone",
 	positionV3 = vector3(),
@@ -6,19 +7,19 @@ return
 	children = {
 	--body
 	{	robotTypeS = "pipuck",
-		positionV3 = vector3(-dis, dis, 0),
+		positionV3 = vector3(-dis, dis, -height),
 		orientationQ = quaternion(0, vector3(0,0,1)),
 	},
 	{	robotTypeS = "pipuck",
-		positionV3 = vector3(dis, dis, 0),
+		positionV3 = vector3(dis, dis, -height),
 		orientationQ = quaternion(0, vector3(0,0,1)),
 	},
 	{	robotTypeS = "pipuck",
-		positionV3 = vector3(-dis, -dis, 0),
+		positionV3 = vector3(-dis, -dis, -height),
 		orientationQ = quaternion(0, vector3(0,0,1)),
 	},
 	{	robotTypeS = "pipuck",
-		positionV3 = vector3(dis, -dis, 0),
+		positionV3 = vector3(dis, -dis, -height),
 		orientationQ = quaternion(0, vector3(0,0,1)),
 	},
 }}
