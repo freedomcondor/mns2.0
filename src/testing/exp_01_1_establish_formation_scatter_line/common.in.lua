b4724b7374af35c7bbeedb2b58ebbc20aa366462
Weixu Zhu <freedomcondor@126.com>
2022-05-26 18:49:16 +0200

    adjust exp01

--- src/testing/exp_01_1_establish_formation_scatter_line/common.in.lua
+++ src/testing/exp_01_1_establish_formation_scatter_line/common.in.lua
@@ -45,7 +45,7 @@ function reset()
 	{ type = "sequence", children = {
 		vns.create_preconnector_node(vns),
 		vns.create_vns_core_node(vns),
-		vns.Driver.create_driver_node(vns, {waiting = true}),
+		vns.Driver.create_driver_node(vns, {waiting = "spring"}),
 	}}
 end
 
