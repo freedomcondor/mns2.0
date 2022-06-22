f0cad5ef703e687c1c578d9593d9228b59765c6a
Weixu Zhu <freedomcondor@126.com>
2022-06-22 23:20:14 +0200

    Add hw exp data for exp03_1

--- src/testing/exp_03_1_obstacle_avoidance_small_hor/morphology.lua
+++ src/testing/exp_03_1_obstacle_avoidance_small_hor/morphology.lua
@@ -4,11 +4,11 @@ local pipuckDis = 0.7
 local droneDis = 1.5
 local height = 1.8
 
-local morph = MorphologyCommon.create_1drone_4pipuck_left_right_hex_node(droneDis, pipuckDis, height, vector3(), quaternion(), "right_double_end")
+local morph = MorphologyCommon.create_1drone_4pipuck_square_node(droneDis, pipuckDis, height, vector3(), quaternion(), "right_double_end")
 morph.children[1].reference = true
 
 table.insert(morph.children, 
-             MorphologyCommon.create_1drone_4pipuck_left_right_hex_node(droneDis, pipuckDis, height, vector3(0, droneDis, 0), quaternion(), "left_double_end")
+             MorphologyCommon.create_1drone_2pipuck_square_node(droneDis, pipuckDis, height, vector3(0, droneDis, 0), quaternion(-math.pi/2, vector3(0,0,1)), "left_double_end")
             )
 
 return morph
