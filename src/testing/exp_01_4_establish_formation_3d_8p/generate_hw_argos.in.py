eefa77db2a3f8814ee6a6b65a772aa81cf6c6c43
Weixu Zhu <freedomcondor@126.com>
2022-06-24 22:44:53 +0200

    Add scenario for 3 drones

---
+++ src/testing/exp_01_4_establish_formation_3d_8p/generate_hw_argos.in.py
@@ -0,0 +1,44 @@
+createArgosFileName = "@CMAKE_SOURCE_DIR@/scripts/createArgosScenario.py"
+#execfile(createArgosFileName)
+exec(compile(open(createArgosFileName, "rb").read(), createArgosFileName, 'exec'))
+
+params = '''
+    script="common.lua"
+    stabilizer_preference_robot="pipuck1"
+    stabilizer_preference_brain="drone1"
+
+    connector_waiting_count="5"
+    connector_waiting_parent_count="8"
+    connector_unseen_count="20"
+    connector_heartbeat_count="10"
+    
+    pipuck_label_from="1"
+    pipuck_label_to="20"
+    block_label_from="25"
+    block_label_to="35"
+
+    obstacle_match_distance="0.30"
+    obstacle_unseen_count="10"
+
+    dangerzone_pipuck="0.35"
+    dangerzone_block="0.35"
+    safezone_drone_drone="3.0"
+    safezone_drone_pipuck="1.5"
+    dangerzone_drone="2"
+
+'''
+
+# generate argos file
+generate_argos_file("@CMAKE_SOURCE_DIR@/scripts/argos_templates/drone_hw.argos", 
+                    "@CMAKE_CURRENT_BINARY_DIR@/hw/01_drone.argos",
+	[
+		["PARAMS",       "driver_default_speed=\"0.03\"\n" + params],  
+	]
+)
+
+generate_argos_file("@CMAKE_SOURCE_DIR@/scripts/argos_templates/pipuck_hw.argos", 
+                    "@CMAKE_CURRENT_BINARY_DIR@/hw/01_pipuck.argos",
+	[
+		["PARAMS",       params],  
+	]
+)
