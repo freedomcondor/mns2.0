2e9c6b0897aa5fe17fb626458987197eefb228b5
Weixu Zhu <freedomcondor@126.com>
2022-07-19 23:51:47 +0200

    Add group start hw exp

---
+++ src/testing/exp_0_hw_10_formation_1_2d_6p_group_start/common.in.lua
@@ -0,0 +1,127 @@
+local myType = robot.params.my_type
+
+package.path = package.path .. ";@CMAKE_SOURCE_DIR@/core/api/?.lua"
+package.path = package.path .. ";@CMAKE_SOURCE_DIR@/core/utils/?.lua"
+package.path = package.path .. ";@CMAKE_SOURCE_DIR@/core/vns/?.lua"
+package.path = package.path .. ";@CMAKE_CURRENT_BINARY_DIR@/?.lua"
+
+pairs = require("AlphaPairs")
+ExperimentCommon = require("ExperimentCommon")
+-- includes -------------
+logger = require("Logger")
+local api = require(myType .. "API")
+local VNS = require("VNS")
+local BT = require("BehaviorTree")
+logger.enable()
+logger.disable("Allocator")
+logger.disable("Stabilizer")
+logger.disable("droneAPI")
+
+-- datas ----------------
+local bt
+--local vns
+local gene = require("morphology")
+
+-- VNS option
+-- VNS.Allocator.calcBaseValue = VNS.Allocator.calcBaseValue_oval -- default is oval
+
+-- argos functions -----------------------------------------------
+--- init
+function init()
+	api.linkRobotInterface(VNS)
+	api.init() 
+	vns = VNS.create(myType)
+	reset()
+end
+
+--- reset
+function reset()
+	vns.reset(vns)
+	if vns.idS == robot.params.stabilizer_preference_brain then vns.idN = 1 end
+	vns.setGene(vns, gene)
+	vns.setMorphology(vns, gene)
+
+	bt = BT.create
+	{ type = "sequence", children = {
+		vns.create_preconnector_node(vns),
+		create_led_node(vns),
+		vns.create_vns_core_node(vns),
+		create_head_navigate_node(vns),
+		vns.Driver.create_driver_node(vns, {waiting = "spring"}),
+	}}
+end
+
+--- step
+function step()
+	-- prestep
+	--logger(robot.id, "-----------------------")
+	api.preStep()
+	vns.preStep(vns)
+
+	-- step
+	bt()
+
+	-- poststep
+	vns.postStep(vns)
+	api.postStep()
+
+	vns.logLoopFunctionInfo(vns)
+	-- debug
+	api.debug.showChildren(vns)
+	--if vns.parentR == nil then
+		--api.debug.showObstacles(vns)
+	--end
+end
+
+--- destroy
+function destroy()
+	api.destroy()
+end
+
+-- drone avoider led node ---------------------
+function create_led_node(vns)
+	return function()
+		-- signal led
+		if vns.robotTypeS == "drone" then
+			local flag = false
+			for idS, robotR in pairs(vns.connector.seenRobots) do
+				if robotR.robotTypeS == "drone" then
+					flag = true
+				end
+			end
+			if flag == true then
+				robot.leds.set_leds("green")
+			else
+				robot.leds.set_leds("red")
+			end
+		end
+		return false, true
+	end
+end
+
+-- drone navigation node ---------------------
+function create_head_navigate_node(vns)
+	local marker_type = 33
+	local obstacle_type = 34
+return function()
+	-- only run for brain
+	if vns.parentR == nil then 
+		for id, ob in ipairs(vns.avoider.obstacles) do
+			if ob.type == marker_type then
+				vns.setGoal(vns, ob.positionV3 - vector3(0.7, 0.5, 0), ob.orientationQ * quaternion(math.pi/2, vector3(0,0,1)))
+			--[[
+			if vns.goal.positionV3:length() < 0.3 then
+				state = 5
+			end
+			--]]
+				return false, true
+			end
+		end
+	elseif vns.robotTypeS == "pipuck" then
+		for id, ob in ipairs(vns.avoider.obstacles) do
+			if ob.positionV3:length() < 0.5 then
+				vns.goal.transV3 = vns.goal.transV3 + vector3(-0.02, 0, 0):rotate(ob.orientationQ)
+			end
+		end
+	end
+end end
---
+++ src/testing/exp_0_hw_10_formation_1_2d_6p_group_start/run.in.py
@@ -0,0 +1,109 @@
+createArgosFileName = "@CMAKE_SOURCE_DIR@/scripts/createArgosScenario.py"
+#execfile(createArgosFileName)
+exec(compile(open(createArgosFileName, "rb").read(), createArgosFileName, 'exec'))
+
+import os
+import math 
+
+# drone and pipuck
+drone_locations = generate_random_locations(2,                  # total number
+                                            0.5, 0,             # origin location
+                                            -3, 3,              # random x range
+                                            -2, 2,              # random y range
+                                            1.5, 1.7)           # near limit and far limit
+pipuck_locations = generate_random_locations(6,                  # total number
+                                            None, None,
+                                            -0.3, 0.3,          # random x range
+                                            -0.3, 0.3,          # random y range
+                                            0.2, 0.3)           # near limit and far limit
+drone_xml = generate_drones(drone_locations, 1)                 # from label 1 generate drone xml tags
+
+pipuck_xml = generate_pipucks(pipuck_locations, 2)             # from label 2 generate pipuck xml tags
+
+x_home = 0
+y_home = 0
+type_home = 34
+
+th = 20
+gate_half = 45
+r = 0.5
+obstacle_payload = 34
+obstacle_marker_payload = 33
+
+obstacle_xml = generate_obstacle_box_xml(
+                                         0,
+                                         r * math.cos(gate_half/180*math.pi) + x_home,
+                                         r * math.sin(gate_half/180*math.pi) + y_home,
+                                         90,
+                                         obstacle_marker_payload
+                                        )
+obstacle_xml += generate_obstacle_box_xml(
+                                         1,
+                                         r * math.cos((360-gate_half)/180*math.pi) + x_home,
+                                         r * math.sin((360-gate_half)/180*math.pi) + y_home,
+                                         -90,
+                                         obstacle_payload
+                                        )
+
+alpha = gate_half + th
+number = 2
+while alpha < 360 - gate_half:
+    angle = alpha
+    if alpha < 180 :
+        angle = alpha + 90
+    else :
+        angle = alpha - 90
+    if number != 1 :
+        obstacle_xml = obstacle_xml + generate_obstacle_box_xml(
+                                                     number,
+                                                     r * math.cos(alpha/180*math.pi) + x_home,
+                                                     r * math.sin(alpha/180*math.pi) + y_home,
+                                                     angle,
+                                                     obstacle_payload
+        )
+    alpha = alpha + th
+    number = number + 1
+
+#pipuck_xml = generate_pipuck_xml(1, -3, 0) + \                 # an extra pipuck and pipuck1
+#             generate_pipucks(pipuck_locations, 2)             # from label 2 generate pipuck xml tags
+
+params = '''
+    stabilizer_preference_robot="pipuck1"
+    stabilizer_preference_brain="drone1"
+
+    block_label_from="25"
+    block_label_to="35"
+
+	dangerzone_pipuck="0.70"
+	dangerzone_block="0.70"
+    deadzone_block="0.2"
+    deadzone_pipuck="0.2"
+
+    obstacle_unseen_count="0"
+'''
+# generate argos file
+generate_argos_file("@CMAKE_CURRENT_BINARY_DIR@/vns_template.argos", 
+#                    "@CMAKE_CURRENT_BINARY_DIR@/vns.argos",
+                    "vns.argos",
+    [
+        ["RANDOMSEED",        str(Inputseed)],  # Inputseed is inherit from createArgosScenario.py
+        ["TOTALLENGTH",       str((Experiment_length or 1000)/5)],
+        ["REAL_SCENARIO",     generate_real_scenario_object()],
+        ["DRONES",            drone_xml], 
+        ["PIPUCKS",           pipuck_xml], 
+		["OBSTACLES",         obstacle_xml], 
+        ["PIPUCK_CONTROLLER", generate_pipuck_controller('''
+              script="@CMAKE_CURRENT_BINARY_DIR@/common.lua"
+              my_type="pipuck"
+        ''' + params)],
+        ["DRONE_CONTROLLER", generate_drone_controller('''
+              script="@CMAKE_CURRENT_BINARY_DIR@/common.lua"
+              my_type="drone"
+        ''' + params)],
+        ["SIMULATION_SETUP",  generate_physics_media_loop_visualization("@CMAKE_BINARY_DIR@")],
+    ]
+)
+              #drone_default_height="1.8"
+
+#os.system("argos3 -c @CMAKE_CURRENT_BINARY_DIR@/vns.argos" + VisualizationArgosFlag)
+os.system("argos3 -c vns.argos" + VisualizationArgosFlag)
