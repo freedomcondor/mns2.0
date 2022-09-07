1b8a22265845aa79ce44a3899b5553eacd7349a1
Weixu Zhu <zhuweixu_harry@126.com>
2022-09-07 15:45:51 +0200

    adjust exp hw 01 simu py

--- src/experiments/exp_0_hw_01_formation_1_2d_10p/run.in.py
+++ src/experiments/exp_0_hw_01_formation_1_2d_10p/run.in.py
@@ -9,14 +9,14 @@ drone_locations = generate_random_locations(2,                  # total number
                                             0.5, 0,             # origin location
                                             -3, 3,              # random x range
                                             -2, 2,              # random y range
-                                            1.5, 1.7)           # near limit and far limit
+                                            1.5, 1.6)           # near limit and far limit
 pipuck_locations = generate_slave_locations_with_origin(
                                             10,
                                             drone_locations,
                                             1, 0.7,
                                             -4, 4,              # random x range
                                             -2.0, 2.0,          # random y range
-                                            0.5, 1.0)           # near limit and far limit
+                                            0.5, 0.8)           # near limit and far limit
 drone_xml = generate_drones(drone_locations, 1)                 # from label 1 generate drone xml tags
 
 pipuck_locations.remove(pipuck_locations[0])
