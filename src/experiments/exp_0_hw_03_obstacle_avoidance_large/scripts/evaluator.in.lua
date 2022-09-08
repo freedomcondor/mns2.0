6c82588a526afa50ac8287df5ee7495353156c16
Weixu Zhu <zhuweixu_harry@126.com>
2022-09-08 16:16:02 +0200

    add exp_0_hw_03 for simu

--- src/experiments/exp_0_hw_03_obstacle_avoidance_large/scripts/evaluator.in.lua
+++ src/experiments/exp_0_hw_03_obstacle_avoidance_large/scripts/evaluator.in.lua
@@ -1,6 +1,6 @@
 package.path = package.path .. ";@CMAKE_SOURCE_DIR@/scripts/logReader/?.lua"
 package.path = package.path .. ";@CMAKE_SOURCE_DIR@/core/utils/?.lua"
-package.path = package.path .. ";@CMAKE_CURRENT_BINARY_DIR@/../?.lua"
+package.path = package.path .. ";@CMAKE_CURRENT_BINARY_DIR@/../simu/?.lua"
 
 logger = require("Logger")
 logReader = require("logReader")
@@ -14,9 +14,10 @@ local robotsData = logReader.loadData("./logs")
 local endStep = logReader.getEndStep(robotsData)
 
 logReader.calcSegmentData(robotsData, geneIndex, 1, 200)
-for i = 201, endStep do
+for i = 201, 1200 do
 	logReader.calcSegmentData(robotsData, geneIndex, i, i)
 end
+logReader.calcSegmentData(robotsData, geneIndex, 1201, endStep)
 
 
 logReader.saveData(robotsData, "result_data.txt")
