39cde44325398e753287997b8e74989ee1ad2a24
Weixu Zhu <zhuweixu_harry@126.com>
2022-09-12 19:15:47 +0200

    Adjust exp0hw06

--- src/experiments/exp_0_hw_06_formation_1_3d_14p/scripts/drawLine.in.py
+++ src/experiments/exp_0_hw_06_formation_1_3d_14p/scripts/drawLine.in.py
@@ -2,11 +2,11 @@ drawDataFileName = "@CMAKE_SOURCE_DIR@/scripts/drawData.py"
 #execfile(drawDataFileName)
 exec(compile(open(drawDataFileName, "rb").read(), drawDataFileName, 'exec'))
 
-legend = []
+#legend = []
 for subfolder in getSubfolders("@CMAKE_CURRENT_SOURCE_DIR@/../data") :
-	legend.append(subfolder)
+	#legend.append(subfolder)
 	drawData(readDataFrom(subfolder + "result_data.txt"))
-plt.legend(legend)
+#plt.legend(legend)
 
 #drawData(readDataFrom("result_data.txt"))
 
