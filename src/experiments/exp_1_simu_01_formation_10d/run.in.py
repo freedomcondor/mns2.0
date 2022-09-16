01def912b6f981bc1418cf5379999f1df6139b28
Weixu Zhu <freedomcondor@126.com>
2022-09-16 17:34:00 +0200

    Adjust exp1simu01

--- src/experiments/exp_1_simu_01_formation_10d/run.in.py
+++ src/experiments/exp_1_simu_01_formation_10d/run.in.py
@@ -7,15 +7,15 @@ import os
 # drone and pipuck
 drone_locations = generate_random_locations(10,                  # total number
                                             0.5, 0,             # origin location
-                                            -3, 3,              # random x range
-                                            -2, 2,              # random y range
+                                            -4, 4,              # random x range
+                                            -4, 4,              # random y range
                                             1.5, 1.6)           # near limit and far limit
 pipuck_locations = generate_slave_locations_with_origin(
                                             40,
                                             drone_locations,
                                             1, 0.7,
                                             -4, 4,              # random x range
-                                            -2.0, 2.0,          # random y range
+                                            -4.0, 4.0,          # random y range
                                             0.5, 0.8)           # near limit and far limit
 drone_xml = generate_drones(drone_locations, 1)                 # from label 1 generate drone xml tags
 
@@ -45,11 +45,11 @@ generate_argos_file("@CMAKE_CURRENT_BINARY_DIR@/vns_template.argos",
 		["DRONE_CONTROLLER", generate_drone_controller('''
               script="@CMAKE_CURRENT_BINARY_DIR@/simu/common.lua"
               my_type="drone"
-              drone_tag_detection_rate="1"
-              drone_default_height="1.8"
-              drone_default_start_height="1.8"
               stabilizer_preference_robot="pipuck1"
               stabilizer_preference_brain="drone1"
+              drone_default_height="1.8"
+              drone_default_start_height="1.8"
+              dangerzone_drone="1.3"
         ''')],
 		["SIMULATION_SETUP",  generate_physics_media_loop_visualization("@CMAKE_BINARY_DIR@")],
 	]
