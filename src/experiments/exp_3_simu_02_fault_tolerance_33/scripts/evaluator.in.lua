7c7c001b2ab8129c89e5ce4478d19c5c2ea7d445
Weixu Zhu <freedomcondor@126.com>
2022-09-22 13:11:12 +0200

    Add fault_tolerance exp3simu02

---
+++ src/experiments/exp_3_simu_02_fault_tolerance_33/scripts/evaluator.in.lua
@@ -0,0 +1,45 @@
+package.path = package.path .. ";@CMAKE_SOURCE_DIR@/scripts/logReader/?.lua"
+package.path = package.path .. ";@CMAKE_SOURCE_DIR@/core/utils/?.lua"
+package.path = package.path .. ";@CMAKE_CURRENT_BINARY_DIR@/../simu/?.lua"
+
+logger = require("Logger")
+logReader = require("logReader")
+logger.enable()
+
+require("morphologiesGenerator")
+
+local droneDis = 1.5
+local pipuckDis = 0.7
+local height = 1.8
+local structure1 = create_left_right_line_morphology(2, droneDis, pipuckDis, height)
+local structure2 = create_back_line_morphology(4, droneDis, pipuckDis, height, vector3(), quaternion())
+local structure3 = create_left_right_back_line_morphology(2, droneDis, pipuckDis, height)
+local gene = {
+	robotTypeS = "drone",
+	positionV3 = vector3(),
+	orientationQ = quaternion(),
+	children = {
+		structure1, 
+		structure2, 
+		structure3,
+	}
+}
+
+local geneIndex = logReader.calcMorphID(gene)
+
+local robotsData = logReader.loadData("./logs")
+
+local stage2Step = logReader.checkIDFirstAppearStep(robotsData, structure2.idN, nil)
+local stage3Step = logReader.checkIDFirstAppearStep(robotsData, structure3.idN, nil)
+local stage4Step = logReader.checkIDFirstAppearStep(robotsData, structure1.idN, stage3Step)
+
+print("stage2 start at", stage2Step)
+print("stage3 start at", stage3Step)
+print("stage4 start at", stage4Step)
+
+logReader.calcSegmentData(robotsData, geneIndex, 1, stage2Step - 1)
+logReader.calcSegmentData(robotsData, geneIndex, stage2Step, stage3Step - 1)
+logReader.calcSegmentData(robotsData, geneIndex, stage3Step, stage4Step - 1)
+logReader.calcSegmentData(robotsData, geneIndex, stage4Step, nil)
+
+logReader.saveData(robotsData, "result_data.txt")
