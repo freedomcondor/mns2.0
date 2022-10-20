1f23e3951c6a1040186226275de04cb2f26ff8bd
Weixu Zhu <zhuweixu_harry@126.com>
2022-10-20 22:32:15 +0200

    Adjust plot scripts for exp2simu scalability analyze

--- src/experiments/exp_2_simu_scalability_analyze/scripts/drawScatter.in.py
+++ src/experiments/exp_2_simu_scalability_analyze/scripts/drawScatter.in.py
@@ -14,11 +14,18 @@ def readFormationData(fileName) :
 	for line in file :
 		splits = line.split(' ')
 	file.close()
-	return float(splits[0]), float(splits[1]), float(splits[2])
+	return float(splits[0]), float(splits[1]), float(splits[2]), float(splits[3]), float(splits[4])
+	'''
+	return float(splits[0]),\  # scale
+	       float(splits[1]),\  # averaged error
+	       float(splits[2]),\  # averaged smooth error
+	       float(splits[3]),\  # converge step
+	       float(splits[4])   # recruit step
+	'''
 
-fig, axs = plt.subplots(2, 2)
+fig, axs = plt.subplots(2, 3)
 
-folder = "@CMAKE_CURRENT_SOURCE_DIR@/../data"
+#folder = "@CMAKE_CURRENT_SOURCE_DIR@/../data"
 folder = "/home/harry/code/mns2.0/build/threads_finish"
 scales = []
 comms = []
@@ -48,20 +55,26 @@ axs[0, 1].set_title("calculation cost")
 
 scales = []
 errors = []
+smoothed_errors = []
 converges = []
+recruits = []
 for subfolder in getSubfolders(folder) :
-	scale, error, converge = readFormationData(subfolder + "result_formation_data.txt")
+	scale, error, smoothed_error, converge, recruit = readFormationData(subfolder + "result_formation_data.txt")
 	scales.append(scale)
 	errors.append(error)
+	smoothed_errors.append(smoothed_error)
 	converges.append(converge)
+	recruits.append(recruit)
 
 axs[1, 0].scatter(scales, errors)
+axs[1, 0].scatter(scales, smoothed_errors, color="red")
 axs[1, 0].set_title("position errors")
-#plt.scatter(scales, errors)
-#plt.show()
 
 axs[1, 1].scatter(scales, converges)
-axs[1, 1].set_title("converge time")
-#plt.scatter(scales, converges)
-#plt.show()
+axs[1, 1].scatter(scales, recruits, color="red")
+axs[1, 1].set_title("converge and recruit time")
+
+axs[1, 2].scatter(scales, recruits, color="red")
+axs[1, 2].set_title("zoom in of recruit time")
+
 plt.show()
