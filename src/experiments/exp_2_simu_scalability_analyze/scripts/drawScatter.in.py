c530e60fd2a06f9530dc5957f68e86c995045a02
Weixu Zhu <freedomcondor@126.com>
2023-02-03 22:36:17 +0800

    Adjust drawScatter script for scalability analyze

--- src/experiments/exp_2_simu_scalability_analyze/scripts/drawScatter.in.py
+++ src/experiments/exp_2_simu_scalability_analyze/scripts/drawScatter.in.py
@@ -22,24 +22,83 @@ def readFormationData(fileName) :
 	       float(splits[3]),\  # converge step
 	       float(splits[4])   # recruit step
 	'''
+def boxplot_25_scales(ax, scales, values, color='b') :
+	index = []
+	datas = []
+	for i in range(0, 25) :
+		index.append(i + 1)
+		datas.append([])
 
-fig, axs = plt.subplots(2, 3)
+	i = 0
+	for value in values :
+		scale = scales[i]/5
+		datas[int(scale-1)].append(value)
+		i = i + 1
 
+	'''
+	ax.boxplot(
+	    datas,
+	    boxprops = dict(facecolor=color, color=color),
+	    capprops = dict(color=color),
+	    whiskerprops = dict(color=color),
+	    flierprops = dict(markerfacecolor=color, markeredgecolor=color, marker='.'),
+	    medianprops = dict(color=color),
+	    patch_artist=True
+	) 
+	'''
+	violin_return = ax.violinplot(datas, showmeans=True)
+	violin_returns = [violin_return]
+
+	ax.set_xticks([5, 10, 15, 20, 25], [25, 50, 75, 100, 125])
+
+	# set font and style for violin plot (both top and bottom if existed)
+	for violin in violin_returns :
+		for line in [violin['cbars'], violin['cmins'], violin['cmeans'], violin['cmaxes']] :
+			line.set_linewidth(0.8) # used to be 1.5
+		for line in [violin['cbars'], violin['cmins'], violin['cmaxes']] :
+			line.set_facecolor('grey')
+			line.set_edgecolor('grey')
+		for line in [violin['cmeans']] :
+			line.set_facecolor(color)
+			line.set_edgecolor(color)
+		for pc in violin['bodies']:
+			pc.set_facecolor(color)
+			pc.set_edgecolor(color)
+	
+	return violin_return
+
+#----------------------------------------------------------------------------------
 #folder = "@CMAKE_CURRENT_SOURCE_DIR@/../data"
-folder = "/home/harry/code/mns2.0/build/threads_finish"
+#folder = "/home/harry/code/mns2.0/build/threads_finish"
+folder = "@CMAKE_SOURCE_DIR@/../../mns2.0-data/src/experiments/exp_2_simu_scalability_analyze/data_simu/data"
+
+fig, axs = plt.subplots(2, 2)
+comm_ax = axs[0, 0]
+time_ax = axs[0, 1]
+error_ax = axs[1, 0]
+converge_ax = axs[1, 1]
+
+fontsize = 5
+#-------------------------------------------------------------
+# communication amount
+ # get data
 scales = []
 comms = []
 for subfolder in getSubfolders(folder) :
 	scale, comm = readCommOrTimeData(subfolder + "result_comm_data.txt")
 	scales.append(scale)
 	comms.append(comm)
+ # boxplot data
+boxplot_25_scales(comm_ax, scales, comms)
+comm_ax.set_ylim([0, 1000])
+#comm_ax.set_title("Number of messages per robot per step")
+comm_ax.set_xlabel('Scale: number of robots', fontsize=fontsize)
+comm_ax.set_ylabel('Communication cost: Number of bytes per robot per step', fontsize=fontsize)
+comm_ax.tick_params(axis='x', labelsize=fontsize)
+comm_ax.tick_params(axis='y', labelsize=fontsize)
 
-axs[0, 0].scatter(scales, comms)
-axs[0, 0].set_ylim([0, 1000])
-axs[0, 0].set_title("communication")
-#plt.scatter(scales, comms)
-#plt.show()
-
+#-------------------------------------------------------------
+# calculation cost (calc time per step)
 scales = []
 times = []
 for subfolder in getSubfolders(folder) :
@@ -47,12 +106,16 @@ for subfolder in getSubfolders(folder) :
 	scales.append(scale)
 	times.append(time)
 
-axs[0, 1].scatter(scales, times)
-axs[0, 1].set_ylim([0, 0.3])
-axs[0, 1].set_title("calculation cost")
-#plt.scatter(scales, times)
-#plt.show()
+boxplot_25_scales(time_ax, scales, times)
+time_ax.set_ylim([0, 3.0])
+#time_ax.set_title("calculation cost per step (s)")
 
+time_ax.set_xlabel('Scale: number of robots', fontsize=fontsize)
+time_ax.set_ylabel('Calculation cost: CPU time per robot per step (s)', fontsize=fontsize)
+time_ax.tick_params(axis='x', labelsize=fontsize)
+time_ax.tick_params(axis='y', labelsize=fontsize)
+#-------------------------------------------------------------
+#  read data of position errors and converge time and recruit time
 scales = []
 errors = []
 smoothed_errors = []
@@ -63,18 +126,45 @@ for subfolder in getSubfolders(folder) :
 	scales.append(scale)
 	errors.append(error)
 	smoothed_errors.append(smoothed_error)
-	converges.append(converge)
-	recruits.append(recruit)
+	converges.append(converge / 5)
+	recruits.append(recruit / 5)
 
-axs[1, 0].scatter(scales, errors)
-axs[1, 0].scatter(scales, smoothed_errors, color="red")
-axs[1, 0].set_title("position errors")
+#-------------------------------------------------------------
+# position errors
+#boxplot_25_scales(error_ax, scales, errors)
+#boxplot_25_scales(error_ax, scales, smoothed_errors, "red")
+boxplot_25_scales(error_ax, scales, smoothed_errors)
+error_ax.set_ylim([0, 0.15])
+#error_ax.set_title("position errors")
 
-axs[1, 1].scatter(scales, converges)
-axs[1, 1].scatter(scales, recruits, color="red")
-axs[1, 1].set_title("converge and recruit time")
+error_ax.set_xlabel('Scale: number of robots', fontsize=fontsize)
+error_ax.set_ylabel('Position error (m)', fontsize=fontsize)
+error_ax.tick_params(axis='x', labelsize=fontsize)
+error_ax.tick_params(axis='y', labelsize=fontsize)
+#-------------------------------------------------------------
+# converge time and recruit time
+handle1 = boxplot_25_scales(converge_ax, scales, converges)
+handle2 = boxplot_25_scales(converge_ax, scales, recruits, 'red')
+converge_ax.set_ylim([0, 400])
+#converge_ax.set_title("converge and recruit time")
 
+converge_ax.set_xlabel('Scale: number of robots', fontsize=fontsize)
+converge_ax.set_ylabel('Converge time (s)', fontsize=fontsize)
+converge_ax.tick_params(axis='x', labelsize=fontsize)
+converge_ax.tick_params(axis='y', labelsize=fontsize)
+
+converge_ax.legend([handle1['bodies'][0],
+                    handle2['bodies'][1]],
+                   ['formation converge time',
+                    'SoNS establishment time'],
+    loc="upper left",
+    fontsize="xx-small"
+)
+
+'''
 axs[1, 2].scatter(scales, recruits, color="red")
 axs[1, 2].set_title("zoom in of recruit time")
+'''
 
-plt.show()
+plt.savefig("exp_2_scalability_analyze.pdf")
+#plt.show()
